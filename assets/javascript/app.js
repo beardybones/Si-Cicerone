5667a321298a72830cc14955e605069178da79ef
JaniceNova <janicenovacek2016@gmail.com>
2018-11-27 09:48:34 -0800

    undefined fields are deleted, but commans still remain

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -24,8 +24,25 @@ $("#city_form").submit(function(event){
       }).then(function(response) {
         console.log(response.records);
         for ( var i = 0; i < response.records.length; i++ ) {
-            
+             if (response.records[i].fields.website ===undefined){
+                response.records[i].fields.website = "";}
+            if (response.records[i].fields.name ===undefined){
+                response.records[i].fields.name = "";}
+            if (response.records[i].fields.address1 ===undefined){
+                response.records[i].fields.address1 = "";}
+            if ( response.records[i].fields.city===undefined){
+                 response.records[i].fields.city = "";}
+            if ( response.records[i].fields.state===undefined){
+                 response.records[i].fields.state = "";}
+             if (response.records[i].fields.cat_name===undefined){
+                response.records[i].fields.cat_name = "";}
+            if (response.records[i].fields.style_name===undefined){
+                response.records[i].fields.style_name = "";}
+             if (response.records[i].fields.descript===undefined){
+            response.records[i].fields.descript = "";
+            }
             $(".distillery").append(response.records[i].fields.name + "<br>"+  response.records[i].fields.address1 +", " + response.records[i].fields.city + ", " + response.records[i].fields.state + "<br>" + response.records[i].fields.cat_name + ", " + response.records[i].fields.style_name + "<br><a id='id5' name='link' href='" +response.records[i].fields.website + "'>" +response.records[i].fields.website+ "</a><br>" + response.records[i].fields.descript + "<br><br><br>")
+           
 
 
 
