2ecac75a75622b9c323d29b1d5482189a85297fe
EKS <saadeh.emad@gmail.com>
2018-11-28 11:46:28 -0800

    General housekeeping of white-space and comments

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -58,7 +58,6 @@ $("#location-submit").on("click", function (e) {
     })
 
         .then(function (response) {
-            console.log(response);
             userLng = response.results[0].geometry.location.lng;
             userLat = response.results[0].geometry.location.lat;
 
@@ -70,13 +69,11 @@ $("#location-submit").on("click", function (e) {
             $("#location").hide();
             $("#main-inputs").show();
 
-
             // Added code for an input-submit...Chord review...
+            // On Submit builds the two urls required for the Zomato ajax calls
             $('#input-submit').on('click', function (e) {
                 e.preventDefault();
                 cuisineInput = $('#food-input').val().trim();
-                // console.log('food-input: ' + cuisineInput);
-                // cuisineIput to be populated by #food-input
                 radiusMeters = 25000;
                 zomatoApiKey = '7fd9b4ff24a0fa2eae39b02482c2e9b1';
                 urlOne = 'https://developers.zomato.com/api/v2.1/cuisines?';
@@ -84,12 +81,9 @@ $("#location-submit").on("click", function (e) {
                 urlRadius = '&radius=' + radiusMeters;
 
 
-                // Ajax call to gather cuisine object for the lat/long coordinates
-
+                // Ajax call to Zomato to gather cuisine object for the lat/long coordinates
                 cuisineUrl = urlOne + urlLat + urlLon
                 // console.log(cuisineUrl);
-
-
                 $.ajax({
                     url: cuisineUrl,
                     method: "GET",
@@ -97,9 +91,6 @@ $("#location-submit").on("click", function (e) {
                         "user-key": zomatoApiKey
                     }
                 }).then(function (responseOne) {
-                    console.log(responseOne);
-                    // console.log(responseOne.cuisines.length);
-                    // console.log((responseOne.cuisines[0].cuisine.cuisine_name).toLowerCase());
 
                     var cuisineId;
                     var ct = 0;
@@ -109,16 +100,12 @@ $("#location-submit").on("click", function (e) {
                         // compare our cuisine input to the zomato api
                         if ((responseOne.cuisines[i].cuisine.cuisine_name).toLowerCase() === (cuisineInput).toLocaleLowerCase()) {
                             cuisineId = responseOne.cuisines[i].cuisine.cuisine_id;
-                            // console.log('cuisineID -1 : ' + cuisineId);
-                            // ajax call to gather eatery data based on lat lon coordinates and cuisine
                             var restaurantsArray = [];
                             var urlCuisine = '&cuisines=' + cuisineId;
                             var queryURL = urlTwo + urlLat + urlLon + urlRadius + urlCuisine;
-                            // console.log(urlCuisine);
-                            // console.log(queryURL);
-                            // console.log('cuisine ID: ' + cuisineId);
 
 
+                            // ajax call to Zomato to get restaurants based on location and cuisine and build restaurant name array for comparison with open beer databasd
                             $.ajax({
                                 url: queryURL,
                                 method: "GET",
@@ -138,19 +125,10 @@ $("#location-submit").on("click", function (e) {
                             alert("nothing found");
                             ct = 0;
                         }
-
-
                     }
-
                 })
-
-
             });
         });
-
-
-
-
 });
 
 
@@ -161,11 +139,4 @@ $("#back-button-1").on("click", function (e) {
     $("#location").show();
     $("#main-inputs").hide();
 });
-
-
-
-
-
-
-
 
