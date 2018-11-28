890f6a82542addb342f6056e87bc25c7f11c29d5
Exumc <cordexum@gmail.com>
2018-11-28 12:14:31 -0800

    adding cards in html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,5 @@
+
+// initialize web page
 $("#location").hide();
 $("#main-inputs").hide();
 $("#results").hide();
@@ -14,16 +16,6 @@ var config = {
 firebase.initializeApp(config);
 var database = firebase.database();
 
-var userLocation = "";
-var alcoholInput = "";
-var foodInput = "";
-var userAge = "";
-var googleMapsQueryURL = "";
-var userCoordinates = "";
-$("#inputAge").submit(function (e) {
-    e.preventDefault();
-});
-
 $("#age-submit").on("click", function (e) {
     e.preventDefault();
     // if userAge > 21 verify user
@@ -33,16 +25,16 @@ $("#age-submit").on("click", function (e) {
 
 
     if (diffAge > 21) {
-
-
         // hide the age input and show the location input
         $("#header").hide();
         $("#location").show();
-    } else {
+    } 
+        // if the age is < 21 alert the user and send them away!
+
+    else {
         alert("You are not old Enough Goodbye!");
         window.location.href = "http://www.nick.com"
     }
-    // if the age is < 21 alert the user and do nothing
 })
 
 $("#location-submit").on("click", function (e) {
@@ -51,14 +43,15 @@ $("#location-submit").on("click", function (e) {
     // gather user input
     userLocation = $("#location-input").val().trim();
     googleMapsQueryURL = "https://maps.googleapis.com/maps/api/geocode/json?address=" + userLocation + "&key=AIzaSyB8Eim861DFG-C8nD2Z83vXE1Pbv-kHlwM";
-
-    $.ajax({
+    // googlemaps API call
+    $.ajax({    
         url: googleMapsQueryURL,
         method: "GET"
     })
 
         .then(function (response) {
-            console.log(response);
+
+            // store the longitude and lattitude
             userLng = response.results[0].geometry.location.lng;
             userLat = response.results[0].geometry.location.lat;
 
@@ -70,26 +63,20 @@ $("#location-submit").on("click", function (e) {
             $("#location").hide();
             $("#main-inputs").show();
 
-
             // Added code for an input-submit...Chord review...
             $('#input-submit').on('click', function (e) {
                 e.preventDefault();
                 cuisineInput = $('#food-input').val().trim();
-                // console.log('food-input: ' + cuisineInput);
+
                 // cuisineIput to be populated by #food-input
                 radiusMeters = 25000;
                 zomatoApiKey = '7fd9b4ff24a0fa2eae39b02482c2e9b1';
                 urlOne = 'https://developers.zomato.com/api/v2.1/cuisines?';
                 urlTwo = 'https://developers.zomato.com/api/v2.1/search?';
                 urlRadius = '&radius=' + radiusMeters;
-
+                cuisineUrl = urlOne + urlLat + urlLon
 
                 // Ajax call to gather cuisine object for the lat/long coordinates
-
-                cuisineUrl = urlOne + urlLat + urlLon
-                // console.log(cuisineUrl);
-
-
                 $.ajax({
                     url: cuisineUrl,
                     method: "GET",
@@ -97,27 +84,20 @@ $("#location-submit").on("click", function (e) {
                         "user-key": zomatoApiKey
                     }
                 }).then(function (responseOne) {
-                    console.log(responseOne);
-                    // console.log(responseOne.cuisines.length);
-                    // console.log((responseOne.cuisines[0].cuisine.cuisine_name).toLowerCase());
 
-                    var cuisineId;
-                    var ct = 0;
+                    cuisineId;
+                    ct = 0;
 
                     for (var i = 0; i < responseOne.cuisines.length; i++) {
                         ct++;
                         // compare our cuisine input to the zomato api
                         if ((responseOne.cuisines[i].cuisine.cuisine_name).toLowerCase() === (cuisineInput).toLocaleLowerCase()) {
                             cuisineId = responseOne.cuisines[i].cuisine.cuisine_id;
-                            // console.log('cuisineID -1 : ' + cuisineId);
-                            // ajax call to gather eatery data based on lat lon coordinates and cuisine
-                            var restaurantsArray = [];
-                            var urlCuisine = '&cuisines=' + cuisineId;
-                            var queryURL = urlTwo + urlLat + urlLon + urlRadius + urlCuisine;
-                            // console.log(urlCuisine);
-                            // console.log(queryURL);
-                            // console.log('cuisine ID: ' + cuisineId);
 
+                            // ajax call to gather eatery data based on lat lon coordinates and cuisine
+                            restaurantsArray = [];
+                            urlCuisine = '&cuisines=' + cuisineId;
+                            queryURL = urlTwo + urlLat + urlLon + urlRadius + urlCuisine;
 
                             $.ajax({
                                 url: queryURL,
@@ -138,26 +118,18 @@ $("#location-submit").on("click", function (e) {
                             alert("nothing found");
                             ct = 0;
                         }
-
-
                     }
-
                 })
-
-
+            $("#main-inputs").hide();
+            $("#logo").hide();
+            $("#results").show();
             });
         });
-
-
-
-
 });
 
 
 $("#back-button-1").on("click", function (e) {
     e.preventDefault();
-
-
     $("#location").show();
     $("#main-inputs").hide();
 });
